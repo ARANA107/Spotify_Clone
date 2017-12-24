40c54a50dc9cc45acda0dd976a464edacd0e008e
Aishwarya Rana <ashrana1993@gmail.com>
2017-12-24 11:46:23 -0500

    Separate files: nav now
    Separate files for navbar and now playing bar created.

---
+++ includes/nowPlayingBarContainer.php
@@ -0,0 +1,81 @@
+<div id="nowPlayingBarContainer">
+  <div id="nowPlayingBar">
+
+    <div id="nowPlayingLeft">
+      <div class="content">
+        <span class="albumLink">
+          <img src="https://i.ytimg.com/vi/rb8Y38eilRM/maxresdefault.jpg" class="albumArtwork">
+        </span>
+        <div class="trackInfo">
+
+          <span class="trackName">
+            <span>Happy Birthday!</span>
+          </span>
+
+          <span class="artistName">
+            <span>Ash Douglass</span>
+          </span>
+        </div>
+
+
+      </div>
+    </div>
+
+    <div id="nowPlayingCenter">
+      <div class="content playerControls">
+        <div class="buttons">
+
+          <button class="controlButton shuffle" title="Shuffle Button">
+            <img src="assets/images/icons/shuffle.png" alt="Shuffle">
+          </button>
+
+          <button class="controlButton previous" title="previous Button">
+            <img src="assets/images/icons/previous.png" alt="previous">
+          </button>
+
+          <button class="controlButton play" title="play Button">
+            <img src="assets/images/icons/play.png" alt="play">
+          </button>
+
+          <button class="controlButton pause" title="pause Button" style="display: none;">
+            <img src="assets/images/icons/pause.png" alt="play">
+          </button>
+
+          <button class="controlButton next" title="next Button">
+            <img src="assets/images/icons/next.png" alt="next">
+          </button>
+
+          <button class="controlButton repeat" title="repeat Button">
+            <img src="assets/images/icons/repeat.png" alt="repeat">
+          </button>
+        </div>
+
+        <div class="playbackBar">
+          <span class="progressTime current">0.00</span>
+
+          <div class="progressBar">
+            <div class="progressBarBg">
+              <div class="progress"></div>
+            </div>
+          </div>
+
+          <span class="progressTime remaining">0.00</span>
+        </div>
+      </div>
+    </div>
+
+    <div id="nowPlayingRight">
+      <div class="volumeBar">
+        <button class="controlButton volume" title="Volume button">
+          <img src = "assets/images/icons/volume.png" alt="Volume">
+        </button>
+        <div class="progressBar">
+          <div class="progressBarBg">
+            <div class="progress"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </div>
+</div>
