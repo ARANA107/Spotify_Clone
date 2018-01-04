870b1f8ff19bd2f33d604bf2faf1934f3b04e76b
Aishwarya Rana <ashrana1993@gmail.com>
2018-01-03 22:31:09 -0500

    albumPage, songsPlaying, SongsId added.

--- includes/nowPlayingBarContainer.php
+++ includes/nowPlayingBarContainer.php
@@ -1,19 +1,84 @@
+<?php
+$songQuery = mysqli_query($con,"SELECT id FROM songs ORDER BY RAND() LIMIT 10");
+$resultArray = array();
+while($row=mysqli_fetch_array($songQuery)){
+  array_push($resultArray,$row['id']);
+}
+
+$jsonArray = json_encode($resultArray);
+?>
+
+<script>
+
+$(document).ready(function(){
+  currentPlaylist = <?php echo $jsonArray; ?>;
+  audioElement = new Audio();
+  setTrack(currentPlaylist[0], currentPlaylist,false);
+});
+
+function setTrack(trackId, newPlaylist, play){
+
+  $.post("includes/handlers/ajax/getSongJson.php", { songId: trackId}, function(data){
+
+    var  track = JSON.parse(data);
+
+    $(".trackName span").text(track.title);
+
+    $.post("includes/handlers/ajax/getArtistJson.php", { artistId: track.artist}, function(data){
+      var artist = JSON.parse(data);
+      $(".artistName span").text(artist.name);
+    });
+
+    $.post("includes/handlers/ajax/getAlbumJson.php", { albumId: track.album}, function(data){
+      var album = JSON.parse(data);
+      $(".albumLink img").attr("src", album.artworkPath);
+    });
+
+    audioElement.setTrack(track);
+    playSong();
+  });
+
+  if(play){
+    audioElement.play();
+  }
+
+}
+
+function playSong(){
+  if (audioElement.audio.currentTime == 0){
+    $.post("includes/handlers/ajax/updatePlays.php", {songId: audioElement.currentlyPlaying.id});
+  }
+
+  $(".controlButton.play").hide();
+  $(".controlButton.pause").show();
+  audioElement.play();
+}
+
+function pauseSong(){
+  $(".controlButton.play").show();
+  $(".controlButton.pause").hide();
+  audioElement.pause();
+}
+
+
+</script>
+
 <div id="nowPlayingBarContainer">
   <div id="nowPlayingBar">
 
     <div id="nowPlayingLeft">
       <div class="content">
         <span class="albumLink">
-          <img src="https://i.ytimg.com/vi/rb8Y38eilRM/maxresdefault.jpg" class="albumArtwork">
+          <img src="" class="albumArtwork">
         </span>
         <div class="trackInfo">
 
           <span class="trackName">
-            <span>Happy Birthday!</span>
+            <span></span>
           </span>
 
           <span class="artistName">
-            <span>Ash Douglass</span>
+            <span></span>
           </span>
         </div>
 
@@ -33,12 +98,12 @@
             <img src="assets/images/icons/previous.png" alt="previous">
           </button>
 
-          <button class="controlButton play" title="play Button">
+          <button class="controlButton play" title="play Button" onclick="playSong()">
             <img src="assets/images/icons/play.png" alt="play">
           </button>
 
-          <button class="controlButton pause" title="pause Button" style="display: none;">
-            <img src="assets/images/icons/pause.png" alt="play">
+          <button class="controlButton pause" title="pause Button" style="display: none;" onclick="pauseSong()">
+            <img src="assets/images/icons/pause.png" alt="pause">
           </button>
 
           <button class="controlButton next" title="next Button">
